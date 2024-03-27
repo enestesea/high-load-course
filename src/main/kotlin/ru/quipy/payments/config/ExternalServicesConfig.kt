eb55bd172155e0d2a10fce2a08a4dc688caf7cf1
nastiass <mitnas03@mail.ru>
2024-03-27 11:39:51 +0300

    small fixes

--- src/main/kotlin/ru/quipy/payments/config/ExternalServicesConfig.kt
+++ src/main/kotlin/ru/quipy/payments/config/ExternalServicesConfig.kt
@@ -1,6 +1,5 @@
 package ru.quipy.payments.config
 
-import org.springframework.boot.context.properties.bind.Bindable.listOf
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.quipy.payments.logic.ExternalServiceProperties
@@ -12,67 +11,54 @@ import java.time.Duration
 class ExternalServicesConfig {
     companion object {
         const val PRIMARY_PAYMENT_BEAN = "PRIMARY_PAYMENT_BEAN"
-        const val SECONDARY_PAYMENT_BEAN = "SECONDARY_PAYMENT_BEAN"
 
         // Ниже приведены готовые конфигурации нескольких аккаунтов провайдера оплаты.
         // Заметьте, что каждый аккаунт обладает своими характеристиками и стоимостью вызова.
 
         private val accountProps_1 = ExternalServiceProperties(
-            // most expensive. Call costs 100
-            "test",
-            "default-1",
-            parallelRequests = 10000,
-            rateLimitPerSec = 100,
-            request95thPercentileProcessingTime = Duration.ofMillis(1000),
+                // most expensive. Call costs 100
+                "test",
+                "default-1",
+                parallelRequests = 10000,
+                rateLimitPerSec = 100,
+                request95thPercentileProcessingTime = Duration.ofMillis(1000),
         )
 
         private val accountProps_2 = ExternalServiceProperties(
-            // Call costs 70
-            "test",
-            "default-2",
-            parallelRequests = 100,
-            rateLimitPerSec = 30,
-            request95thPercentileProcessingTime = Duration.ofMillis(10_000),
+                // Call costs 70
+                "test",
+                "default-2",
+                parallelRequests = 100,
+                rateLimitPerSec = 30,
+                request95thPercentileProcessingTime = Duration.ofMillis(10_000),
         )
 
         private val accountProps_3 = ExternalServiceProperties(
-            // Call costs 40
-            "test",
-            "default-3",
-            parallelRequests = 30,
-            rateLimitPerSec = 8,
-            request95thPercentileProcessingTime = Duration.ofMillis(10_000),
+                // Call costs 40
+                "test",
+                "default-3",
+                parallelRequests = 30,
+                rateLimitPerSec = 8,
+                request95thPercentileProcessingTime = Duration.ofMillis(10_000),
         )
 
         // Call costs 30
         private val accountProps_4 = ExternalServiceProperties(
-            "test",
-            "default-4",
-            parallelRequests = 8,
-            rateLimitPerSec = 5,
-            request95thPercentileProcessingTime = Duration.ofMillis(10_000),
+                "test",
+                "default-4",
+                parallelRequests = 8,
+                rateLimitPerSec = 5,
+                request95thPercentileProcessingTime = Duration.ofMillis(10_000),
         )
     }
 
     @Bean(PRIMARY_PAYMENT_BEAN)
-    fun primaryPaymentService() = PaymentExternalServiceImpl(
-        ExternalServiceProperties(
-            "test",
-            "default-1",
-            10000,
-            100,
-            Duration.ofMillis(1000)
-        )
-    )
-
-    @Bean(SECONDARY_PAYMENT_BEAN)
-    fun secondaryPaymentService() = PaymentExternalServiceImpl(
-        ExternalServiceProperties(
-            "test",
-            "default-2",
-            100,
-            30,
-            Duration.ofMillis(10_000)
-        )
-    )
+    fun fastExternalService() =
+            PaymentExternalServiceImpl(
+                    listOf(
+                            accountProps_4,
+                            accountProps_3,
+                            accountProps_2
+                    )
+            )
 }
